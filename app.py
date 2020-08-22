c3755db9188f3fe153bd66e8b48d33bf9783c04a
Anastasia Kiiru <kiiru.anastasia@gmail.com>
2020-08-22 22:50:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -170,3 +170,6 @@ def predict():
     prediction = predict_sale(df, model)
     
     return render_template('index.html', prediction_text='$ {}'.format(prediction))
+
+if __name__ == '__main__':
+    app.run(debug=True)
